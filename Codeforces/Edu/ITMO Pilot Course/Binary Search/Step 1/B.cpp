2af3edbf62de03ea419b097cb553374fa35ef9a6
Sukarna Paul <sukarnapaul1893@gmail.com>
2022-04-14 15:46:56 +0600

    Add files via upload

---
+++ Codeforces/Edu/ITMO Pilot Course/Binary Search/Step 1/B.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+
+using namespace std;
+
+int n,k;
+vector<ll>vec;
+
+void Input();
+
+void Solve(){
+    sort(vec.begin(),vec.end());
+
+    for(int i=0;i<k;i++){
+        int x;
+        cin>>x;
+
+        int ans = upper_bound(vec.begin(),vec.end(),x)-vec.begin();
+
+        if(vec[ans-1]<=x)cout<<ans<<"\n";
+        else            cout<<0<<"\n";
+    }
+}
+
+void Clear(){
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    Input();
+    Solve();
+    Clear();
+
+}
+
+void Input(){
+    cin>>n>>k;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        vec.pb(x);
+    }
+}
