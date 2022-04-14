518cacdcc6f94a92fc924635a3b31edf69b35e2f
Sukarna Paul <sukarnapaul1893@gmail.com>
2022-04-14 15:37:14 +0600

    Add files via upload

---
+++ Codeforces/A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+
+using namespace std;
+
+string s;
+
+void Input();
+
+void Solve(){
+    cout<<s<<"\n";
+}
+
+void Clear(){
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    Input();
+    Solve();
+    Clear();
+
+}
+
+void Input(){
+    getline(cin,s);
+}
